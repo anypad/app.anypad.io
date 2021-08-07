5963828e634d06640296eee13e5df116c9c5a309
anypad <86584934+anypad@users.noreply.github.com>
2021-08-07 20:19:28 +0700

    Add files via upload

---
+++ precache-manifest.5736ba74ae33b6e649b03fe899fc7711.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5409b6a4769a12687037766a042bde80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55e9b561c6f6cc01c202",
+    "url": "/static/css/2.adc0bda4.chunk.css"
+  },
+  {
+    "revision": "a9d427297c581b3ae728",
+    "url": "/static/css/main.6ea08e55.chunk.css"
+  },
+  {
+    "revision": "55e9b561c6f6cc01c202",
+    "url": "/static/js/2.2270ff56.chunk.js"
+  },
+  {
+    "revision": "5b810e607ba38cb28b5f68692f92d6fd",
+    "url": "/static/js/2.2270ff56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b607dce5eed330a0f47",
+    "url": "/static/js/3.504c0cbe.chunk.js"
+  },
+  {
+    "revision": "95b85fc57f7f49a2f3a98124773ccfef",
+    "url": "/static/js/3.504c0cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5557af8118152c926e8e",
+    "url": "/static/js/4.a05d1975.chunk.js"
+  },
+  {
+    "revision": "a9d427297c581b3ae728",
+    "url": "/static/js/main.225a9e0c.chunk.js"
+  },
+  {
+    "revision": "be194e4bf12f16ff96a3",
+    "url": "/static/js/runtime-main.0a2d062b.js"
+  },
+  {
+    "revision": "42c50753a5a268748fa778bff3b4eb51",
+    "url": "/static/media/bsc.42c50753.png"
+  },
+  {
+    "revision": "4c16202afa359885e88484219e5d410b",
+    "url": "/static/media/diamond.4c16202a.png"
+  },
+  {
+    "revision": "58085e45f522ff131c75ff3bcf41abc7",
+    "url": "/static/media/logo.58085e45.png"
+  },
+  {
+    "revision": "ce11ab18edcff476b67dedc6bbf9f6e0",
+    "url": "/static/media/more.ce11ab18.svg"
+  }
+]);
---
+++ static/js/4.a05d1975.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpheco-lauchpad"]=this["webpackJsonpheco-lauchpad"]||[]).push([[4],{1565:function(c,h){},1662:function(c,h){}}]);
+//# sourceMappingURL=4.a05d1975.chunk.js.map
