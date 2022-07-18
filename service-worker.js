28f6371f3110c6ac6d00d8f1ed937e5c9be9f52c
anypad <86584934+anypad@users.noreply.github.com>
2022-07-18 15:41:10 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff85fcfc1a23dc4a866d5d2642d0ae1b.js"
+  "/precache-manifest.6e41cc234b8f1b0c6f2ca2e802d43cc3.js"
 );
 
 self.addEventListener('message', (event) => {
